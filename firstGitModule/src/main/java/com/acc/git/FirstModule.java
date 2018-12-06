9480c4b11aed3e5a593e47cac788cddf599d05b3
JoyAbleCN <xian_sh1en@163.com>
2018-12-06 10:08:16 +0800

    create first module

---
+++ firstGitModule/src/main/java/com/acc/git/FirstModule.java
@@ -0,0 +1,11 @@
+package com.acc.git;
+
+public class FirstModule {
+    public static void main(String[] args) {
+        System.out.println("first");
+        System.out.println("second");
+        System.out.println("third");
+        System.out.println("fourth");
+        System.out.println("fifth");
+    }
+}
